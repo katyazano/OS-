78171ba869e2dd9d60dac496d82b5197383401bb
katyazano <katheryn.azano@gmail.com>
2026-03-12 22:59:33 -0600

    multiprocesamiento

--- ActMB/src/main.cpp
+++ ActMB/src/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 
 using namespace std;
+
 double procesos_mb[3];
 int numProcesos;
 int unidad;
@@ -10,14 +11,15 @@ int arquitectura;
 double fragmentacion;
 double mb;
 double vf;
-double lote;
+double lote = 0; 
 double totalMB;
+int nucleos;
 
 void conversion(){
     for(int i = 0; i < numProcesos; i++) {
-        cout << "\nSeleccione la unidad original (1: KB, 2: MB, 3: GB, 4: TB): ";
+        cout << "\nSeleccione la unidad original (1: KB, 2: MB, 3: GB, 4: TB) para el proceso " << i+1 << ": ";
         cin >> unidad;
-        cout << "\nIngrese el tamano del proceso: ";
+        cout << "Ingrese el tamano del proceso: ";
         cin >> tamano;
 
         if (unidad == 1) {
@@ -43,26 +45,67 @@ double suma(){
 
 void secuencial(){
     double secu = totalMB / vf;
-    cout << "-> TIEMPO PROCESAMIENTO SECUENCIAL: " << secu << endl;
+    cout << "\n-> TIEMPO PROCESAMIENTO SECUENCIAL: " << secu << " segundos" << endl;
     cout << "-> VELOCIDAD FINAL DEL PROCESADOR: " << vf << " MB/s" << endl;
 }
 
 void batch(){
-    cout << "\nIngrese el lote: ";
-    cin >> lote;
+    if (lote <= 0) {
+        cout << "\nIngrese el lote (MB): ";
+        cin >> lote;
+    }
     double batch = totalMB / lote;
     double tBatch = batch / vf;
-    cout << "-> BATCH: " << batch << endl;
-    cout << "-> TIEMPO PROCESAMIENTO BATCH: " << tBatch << endl;
+    cout << "\n-> BATCH (Lotes): " << batch << endl;
+    cout << "-> TIEMPO PROCESAMIENTO BATCH: " << tBatch << " segundos" << endl;
     cout << "-> VELOCIDAD FINAL DEL PROCESADOR: " << vf << " MB/s" << endl;
+}
+
+void multiproceso(){    
+    if (vf <= 0) {
+        cout << "\nError: velocidad final invalida (revise la fragmentacion)." << endl;
+        return;
+    }
+
+    cout << "\nIngrese la cantidad de procesadores (nucleos): ";
+    cin >> nucleos;
+
+    if (nucleos <= 0) {
+        cout << "Error: cantidad de procesadores invalida." << endl;
+        return;
+    }
+
+    int sincronia;
+    cout << "Ingrese tipo de procesamiento (1. Sincronia / 2. Asincronia): ";
+    cin >> sincronia;
+
+    if (sincronia <= 0 || sincronia >= 3) {
+        cout << "Error: opcion de sincronia invalida." << endl;
+        return;
+    } else if (sincronia == 1){
+        nucleos = nucleos; // Sincrono: usan todos
+    } else {
+        nucleos = nucleos - 1; // Asincrono: se reserva uno
+    }
+
+    if (lote <= 0) {
+        cout << "Para el multiprocesamiento, ingrese el tamano del lote (MB): ";
+        cin >> lote;
+    }
+
+    double p_multi = ((totalMB / lote ) / nucleos ) / vf;
+
+    cout << "\n[3] Velocidad Final Global: " << vf << " MB/s" << endl;
+    cout << "[3] Tiempo Multiproceso (" << nucleos << " nucleos activos): " << p_multi << " segundos\n" << endl;
 }
 
 int main() {
     int op;
     cout << "\nIngrese la arquitectura del procesador (1: 16x, 2: 32x, 3: 64x): ";
     cin >> arquitectura;
-    cout << "\nIngrese el porcentaje de fragmentacion de la memoria (0-100): ";
+    cout << "Ingrese el porcentaje de fragmentacion de la memoria (1-100): ";
     cin >> fragmentacion;
+
     do {
         cout << "Ingrese la cantidad de procesos (max 3): ";
         cin >> numProcesos;
@@ -70,35 +113,48 @@ int main() {
 
     conversion();
     totalMB = suma();
-    vf = (arquitectura * fragmentacion)/100;
+
+    int bits_reales = 32;
+    if (arquitectura == 1) bits_reales = 16;
+    else if (arquitectura == 2) bits_reales = 32;
+    else if (arquitectura == 3) bits_reales = 64;
+
+    vf = (bits_reales * fragmentacion) / 100.0;
+    
     cout << fixed << setprecision(4);
 
-    do{
-        cout << R"(---- Simulador de Gestion de Memoria y Procesamientos ----
+    do {
+        cout << R"(
+---- Simulador de Gestion de Memoria y Procesamientos ----
         1. Procesamiento secuencial
         2. Procesamiento por lote
-        3. Multiprocesamiento //tbd
+        3. Multiprocesamiento
         4. Todos
         0. Salir
-        )" << endl;
+Elige una opcion: )";
         cin >> op;
 
-        switch (op)
-        {
-        case 1:
-            secuencial();
-            break;
-        case 2:
-            batch();
-            break;
-        case 3:
-            cout << "tbd" << endl;
-            break;
-        case 0:
-            break;
-        default:
-            cout << "\nOpcion no valida.\n" << endl;
-            break;
+        switch (op) {
+            case 1:
+                secuencial();
+                break;
+            case 2:
+                batch();
+                break;
+            case 3:
+                multiproceso();
+                break;
+            case 4: 
+                secuencial();
+                batch();
+                multiproceso();
+                break;
+            case 0:
+                cout << "\nSaliendo...\n" << endl;
+                break;
+            default:
+                cout << "\nOpcion no valida.\n" << endl;
+                break;
         }
     } while (op != 0);
 
